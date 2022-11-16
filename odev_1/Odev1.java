f67904f8bb5ae8ca865fc36b3f3b9433dcbac3d6
bugraKaan <bugrakaanturkyilmaz@gmail.com>
2022-11-16 15:30:30 +0300

    java ile örnek değişken oluşturma
    Değişken oluşturma ve çeşitli örnek uygulamalar

---
+++ odev_1/Odev1.java
@@ -0,0 +1,149 @@
+package com.example.odev_1;
+
+public class Odev1 {
+
+    public static void main(String[] args) {
+
+        String sehir = "ANKARA";
+        System.out.println("sehir: "+sehir);
+
+        String ulke = "TURKYIYE" ;
+        System.out.println("ulke: "+ulke);
+
+        String telNo ="05411235698" ;
+        System.out.println("telNo: "+telNo);
+
+        int postaKodu = 06200;
+        System.out.println("postaKodu: "+postaKodu);
+
+        String email = "ahmet@gmail.com" ;
+        System.out.println("email: " +email);
+
+        String meslek = "Muhendis";
+        System.out.println("meslek: "+meslek);
+
+        int stokMiktarı = 1200;
+        System.out.println("stokMiktarı: "+stokMiktarı);
+
+        String MusteriAdi= "Veli";
+        System.out.println("MusteriAdi: "+MusteriAdi);
+
+        int bakiye = 200;
+        System.out.println("bakiye: "+bakiye);
+
+        String dogumTarihi = "31/07/1999" ;
+        System.out.println("dogumTarihi: "+dogumTarihi);
+
+        int Maas = 15000;
+        System.out.println("Maas"+Maas+"TL");
+
+        String MedeniDurum = "Bekar" ;
+        System.out.println("MedeniDurum: "+MedeniDurum);
+
+        String urunYorumu = " Guzel " ;
+        System.out.println("urunYorumu: "+urunYorumu);
+
+        String odemeTarihi ="10/12/2022" ;
+        System.out.println("odemeTarihi: "+odemeTarihi);
+
+        float  odeme =  1400 ;
+        System.out.println("odeme: "+odeme+"TL");
+
+        int siparisAdedi = 12;
+        System.out.println("siparisAdedi: "+siparisAdedi);
+
+        String ArabaModeli = "x6";
+        System.out.println("ArabaModeli: "+ArabaModeli);
+
+        String kitapAdı ="Devlet";
+        System.out.println("kitapAdı: "+kitapAdı);
+
+        String yayinTarihi = "12/02/2003";
+        System.out.println("yayinTarihi: "+yayinTarihi);
+
+        int indirimMiktari = 50;
+        System.out.println("indirimMiktari: "+indirimMiktari+"TL");
+
+        int odaSayisi = 3;
+        System.out.println("odaSayisi: "+odaSayisi);
+
+        double enlem = 39.59 ;
+        System.out.println("enlem: "+enlem);
+
+        double boylam = 32.53;
+        System.out.println("boylam: "+boylam);
+
+        String urunAdi = "ayakkabi" ;
+        System.out.println("urunAdi: "+urunAdi);
+
+        int yemekFiyati = 150;
+        System.out.println("yemekFiyati: "+yemekFiyati);
+
+        String Marka = "Adidas";
+        System.out.println("Marka: "+Marka);
+
+        String MuzikAdi = "wallerman";
+        System.out.println("MuzikAdi: "+MuzikAdi);
+
+        int videoSuresi = 15;
+        System.out.println("videoSuresi: "+videoSuresi+"dk");
+
+        Double urunPuani = 4.77;
+        System.out.println("urunPuani: "+urunPuani);
+
+        String ResimAdi ="Yildizli Gece";
+        System.out.println("ResimAdi: "+ResimAdi);
+
+        String DosyaFormati = ".pdf";
+        System.out.println("DosyaFormati: "+DosyaFormati);
+
+        String renk = "Mavi";
+        System.out.println("renk: "+renk);
+
+        String renk_Kodu = "0000ff";
+        System.out.println("renk_Kodu: "+renk_Kodu);
+
+        String telModeli = "Iphone11";
+        System.out.println("telModeli: "+telModeli);
+
+        Double ekranBoyutu = 15.6;
+        System.out.println("ekranBoyutu: "+ekranBoyutu);
+
+        Double agırlık =74.5;
+        System.out.println("agırlık: "+agırlık+"kg");
+
+        String ulusalGun = "23 Nisan Ulusal Egemenlik ve Cocuk Bayrami";
+        System.out.println("ulusalGun: "+ulusalGun);
+
+        String tatilGun = "Pazar";
+        System.out.println("tatilGun: "+tatilGun);
+
+        String rezervasyonTarihi = "11/05/2023";
+        System.out.println("rezervasyonTarihi: "+rezervasyonTarihi);
+
+        String sokakAdi = "Gir sokak";
+        System.out.println("sokakAdi: "+sokakAdi);
+
+        int otobusHatti = 281;
+        System.out.println("otobusHatti: "+otobusHatti);
+
+        int kalanDakika = 10;
+        System.out.println("kalanDakika: "+kalanDakika);
+
+        int takipKodu = 123456789;
+        System.out.println("takipKodu: "+takipKodu);
+
+        String kuponSuresi = "4 saat";
+        System.out.println("kuponSuresi: "+kuponSuresi);
+
+        int kuponKodu = 1235;
+        System.out.println("kuponKodu: "+kuponKodu);
+
+        String fatura_adresi = "12. Cadde Gir Sokak No:11 Mamak/ Ankara ";
+        System.out.println("fatura_adresi: "+fatura_adresi);
+
+    }
+
+
+
+}
